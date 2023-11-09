7fcb40ec46b9786f1a975f84596eaf985d474a57
Daryll <77931825+pdaryll@users.noreply.github.com>
2023-11-09 15:07:27 +0800

    Solve exercise 3
    Was pretty much perfect the logic was the same with the solution. But
    mine was a bit longer because I wanted it to be more readable. But can
    totally be a one liner.

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,7 @@
-const reverseString = function() {
-
+const reverseString = function(word) {
+    let wordArray = word.split('');
+    wordArray.reverse();
+    return wordArray.join('');
 };
 
 // Do not edit below this line
