035a7e7a5885ed7706d67561244a3a0461e6fb8b
Daryll <77931825+pdaryll@users.noreply.github.com>
2023-11-09 18:43:39 +0800

    Solve exercise 7
    Was pretty easy. Nothing really remarkable.

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(f) {
+  let c = (f-32) * (5/9);
+  return Math.round(c * 10)/10
+  // return c.toFixed(1);
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(c) {
+ let f = c * (9/5) + 32;
+ return Math.round(f * 10)/10;
+//  return f.toFixed(1);
 };
 
 // Do not edit below this line
