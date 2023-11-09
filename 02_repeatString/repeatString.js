9cdc10e69ad79e93d71738fb3e10417dab1a64c5
Daryll <77931825+pdaryll@users.noreply.github.com>
2023-11-09 14:53:44 +0800

    Solve exercise 2
    Solved it almost perfectly, logic is pretty much perfect. Very easy.

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(text, number) {
+    let textAnswer = "";
+    if (number < 0){
+        return "ERROR";
+    }
+    for (let index = 0; index < number; index++) {
+        textAnswer += text;
+    }
+    return textAnswer;
 };
 
 // Do not edit below this line
