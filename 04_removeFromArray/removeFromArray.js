f59382585b9ff74521fc95857f82f4c2244fed09
Daryll <77931825+pdaryll@users.noreply.github.com>
2023-11-09 16:24:48 +0800

    Solve exercise 4
    Was pretty difficult for me, didn't know they have ...args function, and
    didn't know how to use for each. Definitely save this for reference.

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,20 @@
 const removeFromArray = function() {
 
+    let array = arguments[0];
+    let numbers = [];
+
+    for (let i = 1; i < arguments.length; i++) {
+        numbers.push(arguments[i]);
+    }
+
+    for (let i = 0; i < numbers.length; i++) {
+        
+        let index = array.indexOf(numbers[i]);
+        if (index >= 0){
+            array.splice(index, 1);
+        }
+    }
+    return array;  
 };
 
 // Do not edit below this line
